7c2af04e9fbd1ac43200fcac6996f4f17c5708b4
bpoc-mark <markariel.maata@bpoc.co.jp>
2023-05-24 17:07:49 +0800

    added validation on textarea field / added scrolltoview when errors display

--- src/components/page/Reservation/ReservationForm.tsx
+++ src/components/page/Reservation/ReservationForm.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, ChangeEvent, KeyboardEvent, useRef } from "react";
 import Button from "../../ui/Button";
 import Steps from "../../ui/Steps";
 import Link from "next/dist/client/link";
@@ -9,35 +9,63 @@ export default function ReservationForm() {
   const [name, setName] = useState('')
   const [phoneNumber, setPhoneNumber] = useState('')
   const [email, setEmail] = useState('')
-  
+  const [value, setValue] = useState("");
   const [validateError, setValidateError] = useState({
     name: false,
     phoneNumber: false,
-    email: false
+    email: false,
+    value: false
   })
+
+  const errorRef = useRef<any>(null);
 
   const submitHandler = (e: any) => {
     e.preventDefault();
 
-    if (name.trim() === '' || phoneNumber.trim() === '' || email.trim() === '') {
+    if (name.trim() === '' || phoneNumber.trim() === '' || email.trim() === '' || value.trim() === '') {
       setValidateError({
         name: name.trim() === '',
         phoneNumber:  phoneNumber.trim() === '',
-        email:  email.trim() === ''
+        email:  email.trim() === '',
+        value:  value.trim() === ''
       })
+      errorRef.current.scrollIntoView({ behavior: 'smooth' });
       return;
     }
 
     setValidateError({
       name: false,
       phoneNumber: false,
-      email: false
+      email: false,
+      value: false
     })
     
     // Form submission logic
     console.log('Form submitted successfully!');
   };
 
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    let inputValue = event.target.value;
+    inputValue = inputValue.slice(0, 200);
+
+    inputValue = sanitizeInput(inputValue);
+
+    const lineBreaks = (inputValue.match(/\n/g) || []).length;
+    if (lineBreaks > 10) return;
+
+    setValue(inputValue);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLTextAreaElement>) => {
+    const lineBreaks =
+      (event.target as HTMLTextAreaElement).value.match(/\n/g)?.length || 0;
+    if (lineBreaks >= 10 && event.key !== "Backspace") event.preventDefault();
+  };
+
+  const sanitizeInput = (input: string): string => {
+    const restrictedChars = /[><"/;:{}=-]/g;
+    return input.replace(restrictedChars, "");
+  };
 
   return (
     <Fragment>
@@ -47,13 +75,13 @@ export default function ReservationForm() {
         className="max-w-[1120px] mx-auto md:mt-16 mt-8 lg:px-5 px-5 pb-24"
       >
         <ReservationDetails />
-        <div className={`border py-3 mt-7 md:mt-10 bg-[#F71B1B1A] ${validateError.name || validateError.phoneNumber || validateError.email ? 'block' : "hidden" }`}>
+        <div  className={`border py-3 mt-7 md:mt-10 bg-[#F71B1B1A] ${validateError.name || validateError.phoneNumber || validateError.email || validateError.value ? 'block' : "hidden" }`}>
           <h5 className="text-center font-semibold text-[#F71B1BB2] mb-2">入力を完了させてください</h5>
           <div className="flex justify-center">
           <p className="text-left font-normal text-[13px]">次へ進むには下記の入力内容を確認し、<br className="sm:hidden"></br>修正してください。</p>
           </div>
         </div>
-        <p className="font-bold text-lg mt-6">ご来店者情報</p>
+        <p ref={errorRef} className="font-bold text-lg mt-6">ご来店者情報</p>
         <div className="md:flex mt-5 mb-[3px]">
           <div className="md:w-2/6 md:mr-[25px] mb-3 md:mb-0 md:bg-[#EDEDED] flex items-center">
             <label className="block leading-[19px] md:px-5" htmlFor="name">
@@ -74,8 +102,8 @@ export default function ReservationForm() {
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
-            <p className={`text-[13px] text-[#F71B1B] leading-tight pt-1 ${validateError.name ? 'block' : 'hidden'} `}>
-              ！ お名前を正しく入力してください
+            <p  className={`text-[13px] text-[#F71B1B] leading-tight pt-1 ${validateError.name ? 'block' : 'hidden'} `}>
+            ！ お名前を正しく入力してください
             </p>
           </div>
         </div>
@@ -103,7 +131,7 @@ export default function ReservationForm() {
               onChange={(e) => setPhoneNumber(e.target.value)}
             />
             <p className={`text-[13px] text-[#F71B1B] leading-tight pt-1 ${validateError.phoneNumber ? 'block' : 'hidden'} `}>
-              ！ お名前を正しく入力してください
+            ！ 電話番号を正しく入力してください
             </p>
             <span className="text-[11px] block mt-1">
               ※お店から連絡を差し上げることもございますので、携帯電話・スマートフォンなど連絡の取りやすい番号を入力してください。
@@ -134,7 +162,7 @@ export default function ReservationForm() {
               onChange={(e) => setEmail(e.target.value)}
             />
             <p className={`text-[13px] text-[#F71B1B] leading-tight pt-1 ${validateError.email ? 'block' : 'hidden'} `}>
-              ！ お名前を正しく入力してください
+            ！ メールアドレスを正しく入力してください
             </p>
             <span className="text-[11px] text-[#F71B1B] block mt-2">
               ※ご予約内容をお送りしますので、必ず連絡が取れるメールアドレスをご入力ください。
@@ -151,9 +179,20 @@ export default function ReservationForm() {
               必須
             </span>
           </label>
-          <TextArea rows={7} name="request" />
+          <textarea
+            className="bg-white rounded border border-[#757575] p-3 max-w-[634px] w-full h-full text-sm"
+            placeholder=""
+            name={name}
+            rows={7}
+            value={value}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          ></textarea>
+          <p className={`text-[13px] text-[#F71B1B] leading-tight pt-1 ${validateError.value} ${validateError.value ? 'block' : 'hidden'} `}>
+          ！ ご要望は500文字以内で入力してください
+          </p>
         </div>
-        <p className="text-xs md:text-sm">
+        <p className="text-xs md:text-sm mt-2">
           ※メールでの返信を希望される場合であっても店舗によっては電話連絡となることをご了承ください。
           <br className="md:hidden" />
           また、内容によってはご要望に添えない場合がございます。
