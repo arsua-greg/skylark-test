56288ff200446da3f9ec0e3528a65d755d8210b7
gregarsua <arsuagreg@gmail.com>
2023-05-25 17:56:40 +0800

    add design #3

--- src/components/ui/ConfirmEmailModal.tsx
+++ src/components/ui/ConfirmEmailModal.tsx
@@ -1,31 +1,40 @@
 import Button from "./Button";
+import { Fragment } from "react";
 
 const ConfirmEmailModal = () => {
   return (
-    <div className="bg-white">
-      <label htmlFor="my-modal-3" className="btn">
-        open modal
-      </label>
-      <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box relative">
+    <Fragment>
+      <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+      <div className="modal bg-[#0000008A] drop-shadow-sm">
+        <div className="modal-box relative max-w-[823px] md:max-h-[500px] mx-auto md:p-14 px-4 py-16 rounded-lg">
           <label
-            htmlFor="my-modal-3"
-            className="btn btn-sm btn-circle absolute right-2 top-2"
+            htmlFor="my-modal-4"
+            className="btn btn-sm btn-circle absolute right-2 top-2 bg-[#04512A]"
           >
             ✕
           </label>
-          <h3 className="text-lg font-bold">
-            下記メールアドレスに認証メールを送信します。
-          </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
+          <div className="md:max-w-[485px] mx-auto">
+            <div className="mb-7 text-center">
+              <p className="md:mb-10 mb-7 md:text-base text-sm">
+                下記メールアドレスに認証メールを送信します。
+              </p>
+              <p className="md:text-xl">skylark@skylark.co.jp</p>
+            </div>
+            <Button
+              text="認証メールを送信する"
+              className="md:py-5 h-auto md:text-xl text-lg"
+            />
+            <p className="text-[13px] md:text-sm md:mt-12 mt-8 md:leading-[25px]">
+              ※まだ予約は完了しておりません。
+              <br className="hidden md:visible" />
+              認証メール受信後の申し込み処理が行われなかった場合、ご登録いただいた
+              <br className="hidden md:visible" />
+              内容は無効となりますのでご注意ください。
+            </p>
+          </div>
         </div>
-        <Button text="認証メールを送信する" />
       </div>
-    </div>
+    </Fragment>
   );
 };
 
--- src/pages/reservation/confirmation.tsx
+++ src/pages/reservation/confirmation.tsx
@@ -1,15 +1,90 @@
-import { Fragment } from "react";
-import Button from "../../components/ui/Button";
+import ReservationDetails from "@/components/page/Reservation/ReservationDetails";
+import Steps from "@/components/ui/Steps";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import ConfirmEmailModal from "@/components/ui/ConfirmEmailModal";
 
-export default function ConfirmPage() {
+const ConfirmPage = () => {
+  const router = useRouter();
+
+  const formSubmitHandler = (e: any) => {
+    e.preventDefault();
+  };
+
+  const selectedDate = router.query.selectedDate?.toString() || "";
+
   return (
-    <Fragment>
-      <div className="alert border border-[#FFCD29] bg-[#FFCD291A] max-w-[787px] mx-auto rounded-none justify-center">
-        <p className="text-base text-center">
-          メールアドレスの入力間違い防止の為、次の画面でメールアドレス認証を行います。
-        </p>
-      </div>
-      <Button text="この内容で確定する" />
-    </Fragment>
+    <div className="mt-16">
+      <Steps />
+      <form
+        onSubmit={formSubmitHandler}
+        className="max-w-[1120px] mx-auto md:mt-16 mt-8 lg:px-5 px-5 md:pb-24 pb-6"
+      >
+        <ReservationDetails selectedDate={selectedDate} />
+        <div className="flex justify-between items-center mt-6">
+          <p className="font-bold text-lg">ご来店者情報</p>
+          <Link
+            href={"/"}
+            className="underline underline-offset-4 text-[#04512A]"
+          >
+            変更する
+          </Link>
+        </div>
+        <div className="md:flex mt-5 mb-[3px]">
+          <div className="md:w-2/6 md:mr-[25px] mb-3 md:mb-0 flex items-center">
+            <p className="block leading-[19px] md:px-5">お名前</p>
+          </div>
+          <div className="md:w-8/12 py-2">
+            <p>予約　太郎</p>
+          </div>
+        </div>
+        <div className="md:flex md:mb-[3px] md:mt-0 mt-[23px]">
+          <div className="md:w-2/6 md:mr-[25px] mb-3 md:mb-0 flex items-center">
+            <p className="block md:px-5 leading-[19px] w-full">電話番号</p>
+          </div>
+          <div className="md:w-8/12 py-2">
+            <p>08012345678</p>
+          </div>
+        </div>
+        <div className="md:flex md:mt-0 mt-[23px]">
+          <div className="md:w-2/6 md:mr-[25px] mb-3 md:mb-0 flex items-center">
+            <p className="block md:px-5 leading-[19px] w-full">
+              メールアドレス
+            </p>
+          </div>
+          <div className="md:w-8/12 py-3">
+            <p>skylark@skylark.co.jp</p>
+          </div>
+        </div>
+        <div className="mt-6">
+          <div className="flex justify-between items-center">
+            <p className="block font-bold text-lg">ご要望</p>
+            <Link
+              href={"/"}
+              className="underline underline-offset-4 text-[#04512A]"
+            >
+              変更する
+            </Link>
+          </div>
+          <p className="mt-5 ml-6">
+            アニバーサリーケーキを持ってくるタイミングはこちらが合図した時でお願いしたいのですが、可能でしょうか。
+          </p>
+        </div>
+        <div className="alert border border-[#FFCD29] bg-[#FFCD291A] max-w-[787px] mx-auto rounded-none justify-center mb-12 mt-20">
+          <p className="text-base text-center">
+            メールアドレスの入力間違い防止の為、次の画面でメールアドレス認証を行います。
+          </p>
+        </div>
+        <label
+          htmlFor="my-modal-4"
+          className="btn flex mx-auto rounded-md bg-[#04512A] border-none text-white text-xl sm:max-w-[612px] max-w-[280px] w-full font-normal"
+        >
+          認証メールを送信する
+        </label>
+        <ConfirmEmailModal />
+      </form>
+    </div>
   );
-}
+};
+
+export default ConfirmPage;
