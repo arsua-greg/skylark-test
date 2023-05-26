93789f922cc09f99b605c390f236ef84e0979604
gregarsua <arsuagreg@gmail.com>
2023-05-26 18:04:50 +0800

    add faq page

---
+++ src/pages/faq/index.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Link from "next/link";
+
+const FaqPage = () => {
+  const breadCrumbSp = "<戻る";
+
+  return (
+    <div>
+      <Link href="/" className="text-[#04512A] text-lg mb-5 block">
+        <p className="md:hidden block">{breadCrumbSp}</p>
+        <p className="text-xl md:block hidden text-black">
+          <span className="text-[#04512A]">来店予約</span>　/　よくある質問
+        </p>
+      </Link>
+      <h1 className="md:text-3xl text-lg font-bold text-center">
+        よくある質問
+      </h1>
+      <p className="md:text-base text-[13px] md:mt-8 mt-6">
+        予約サイトについてよくある質問をまとめました
+      </p>
+    </div>
+  );
+};
+
+export default FaqPage;
