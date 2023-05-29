5b5e9d6ff681a74a9e8e7ecf6f7463c1dc9e0f77
gregarsua <arsuagreg@gmail.com>
2023-05-29 13:48:50 +0800

    add state for complete page and adjust margin

--- src/globalState/globalState.ts
+++ src/globalState/globalState.ts
@@ -1,1 +1,13 @@
 import { atom } from "recoil";
+
+export const reservationFormState = atom({
+  key: "reservationFormState",
+  default: {
+    counterValue: "",
+    selectedTimeValue: "",
+    selectedDateValue: "",
+    selectedQuantityValue: "",
+    selectedOfferTime: "",
+    selectedOfferTiming: "",
+  },
+});
