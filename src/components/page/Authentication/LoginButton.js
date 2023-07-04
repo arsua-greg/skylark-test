39ce42c4df87286e7e120bd3b7978236a37df83e
arsuagreg <gregarsua@bpoc.co.jp>
2023-07-04 22:03:58 +0800

    revert the changes on calendar display

--- src/components/page/Authentication/LoginButton.js
+++ src/components/page/Authentication/LoginButton.js
@@ -1,8 +1,17 @@
 import Link from "next/link";
+import auth0 from "auth0-js";
 
 function LoginButton() {
+  var webAuth = new auth0.WebAuth({
+    clientID: "{yourClientId}",
+    domain: "{yourDomain}",
+    redirectUri: "http://example.com",
+    responseType: "token id_token",
+  });
+
   return (
     <div>
+      <h1>Login With PasswordLess Method</h1>
       <Link className="btn btn-accent" href="/api/auth/login">
         Login
       </Link>
