8c5f016d1af14fdd25e88468d6cd50fd4cfa2261
arsuagreg <gregarsua@bpoc.co.jp>
2023-07-04 14:17:23 +0800

    remove commented code

--- src/pages/shops/[shopId].tsx
+++ src/pages/shops/[shopId].tsx
@@ -102,12 +102,6 @@ const HomePage: React.FC<MyPageProps> = ({ initialBookedTableSlot }) => {
 
   const updateButtonState = (option: string, checked: boolean) => {
     setIsButtonDisabled(option === "選択してください");
-
-    // if (option !== "選択してください" && checked) {
-    //   setIsButtonDisabled(false);
-    // } else {
-    //   setIsButtonDisabled(true);
-    // }
   };
 
   const handleTimeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
